6e604330543324461867694ef9e4290118605183
kynhanht <kynhanht@gmail.com>
2023-03-27 16:37:41 +0700

    fix config

--- config-overrides.js
+++ config-overrides.js
@@ -1,6 +1,7 @@
 /* config-overrides.js */
-const { override, useBabelRc } = require("customize-cra");
+const { override, useBabelRc } = require('customize-cra');
 
 module.exports = override(
-    useBabelRc()
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    useBabelRc(),
 );
